b3dcdcf121bd92a3d5002b7eb87aaa31e54d5c7f
jjksy <318thdus@naver.com>
2021-01-27 07:37:14 +0900

    02동적>정적으로

--- 002_Make/main.js
+++ 002_Make/main.js
@@ -1,14 +1,17 @@
-var btnNew = document.getElementById("btnAdd"); //버튼연결
-// btnNew.onclick = addNewItem; //onclick이벤트 추가, addNewItem 함수 call
-btnNew.onclick = function () {
-  addNewItem(document.getElementById("todolist"));
-};
-
-function addNewItem(list) {
+function addNewItem(list, itemText) {
   // 함수생성
   var listItem = document.createElement(`li`); //li 태그 생성 이벤트를 listItem 변수에 담기
-  listItem.innerHTML = "Hi"; // li 태그 안에 innerHtml 메소드로 문자열 입력
+  listItem.innerText = itemText; // input 태그로부터 들어온 값 넣어주기
 
   //   var list = document.getElementById("todolist");
   list.appendChild(listItem); //appenChild 메소드로 listItem 닫기
 }
+var btnNew = document.getElementById("btnAdd"); //버튼연결
+// btnNew.onclick = addNewItem; //onclick이벤트 추가, addNewItem 함수 call
+btnNew.onclick = function () {
+  var inputText = document.getElementById("inputText");
+  var itemText = inputText.value;
+
+  if (!itemText || itemText === "" || itemText === "") return false; //blank 방지
+  addNewItem(document.getElementById("todolist"), itemText);
+};
