0ff52247e210f3c27af798ca9ef3ddde7d33e1c2
jjksy <318thdus@naver.com>
2021-01-27 07:25:51 +0900

    주석 추가

--- 002_Make/main.js
+++ 002_Make/main.js
@@ -1,10 +1,11 @@
-var btnNew = document.getElementById("btnAdd");
-btnNew.onclick = addNewItem;
+var btnNew = document.getElementById("btnAdd"); //버튼연결
+btnNew.onclick = addNewItem; //onclick이벤트 추가, addNewItem 함수 call
 
 function addNewItem() {
-  var listItem = document.createElement(`li`);
-  listItem.innerHTML = "Hi";
+  // 함수생성
+  var listItem = document.createElement(`li`); //li 태그 생성 이벤트를 listItem 변수에 담기
+  listItem.innerHTML = "Hi"; // li 태그 안에 innerHtml 메소드로 문자열 입력
 
   var list = document.getElementById("todolist");
-  list.appendChild(listItem);
+  list.appendChild(listItem); //appenChild 메소드로 listItem 닫기
 }
